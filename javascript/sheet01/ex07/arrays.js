37e253462e71f6ae7d089fcd0fe8108bec6ccdae
jlvalladares <jvalladaresreina@gmail.com>
2024-11-12 13:57:11 +0100

    WIP: arreglos

---
+++ javascript/sheet01/ex07/arrays.js
@@ -0,0 +1,6 @@
+const diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+for (i = 0; i < diasSemana.length; i++) {
+	console.log(diasSemana[i]);
+}
+
+function sumarArreglo() {}
