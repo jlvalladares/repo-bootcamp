a202349be92c42eacd73b543e91b2e6e548c832f
jlvalladares <jvalladaresreina@gmail.com>
2024-11-12 11:51:06 +0100

    Implementado condicionales

---
+++ javascript/sheet01/ex04/script.js
@@ -0,0 +1,8 @@
+let numero = prompt("introduce el numero: ");
+if (numero < 0) {
+	console.log("El numero ", numero, " es negativo");
+} else if (numero > 0) {
+	console.log("El numero ", numero, " es positivo");
+} else {
+	console.log("El numero ", numero, " es cero");
+}
