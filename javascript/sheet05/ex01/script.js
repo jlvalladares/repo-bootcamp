62556031458f18e715c0370da0743ea4662bbf45
jlvalladares <jvalladaresreina@gmail.com>
2024-11-20 23:55:10 +0100

    WIP buscador en su rama

--- javascript/sheet05/ex01/script.js
+++ javascript/sheet05/ex01/script.js
@@ -1,23 +1,37 @@
-const listaPokemon = document.querySelector("#listaPokemon");
-let URL = "https://pokeapi.co/api/v2/pokemon/";
+document.addEventListener('DOMContentLoaded', () => {
+  mostrarPokemon();
+  buscarPokemon();
+})
 
-for (let i = 1; i <= 151; i++) {
-  fetch(URL + i)
-    .then((response) => response.json())
-    .then((data) => mostrarPokemon(data));
+
+
+const listaPokemon = document.querySelector("#listaPokemon");
+
+async function fetchData(url) {
+	const response = await fetch(url);
+	const json = await response.json();
+	return json;
 }
 
+async function obtenerDatos() {
+	let URL = "https://pokeapi.co/api/v2/pokemon/";
+	for (let i = 1; i <= 151; i++) {
+		const pokemonData = await fetchData(`${URL}/${i}/`);
+		const specieURL = pokemonData.species.url;
+		const evolucion = await fetchData(specieURL);
+		pokemonData.evolucion = evolucion.evolves_from_species?.name;
+		mostrarPokemon(pokemonData);
+	}
+}
+
+obtenerDatos();
+
 function mostrarPokemon(poke) {
-  let tipos = poke.types.map((type) => `<li class="${type.type.name}">${type.type.name}</li>`);
-  tipos = tipos.join("");
-
-  let evolucion = poke.name.id - 1;
-
-  const div = document.createElement("div");
-  div.classList.add("pokemon");
-  div.innerHTML = `
-
-   
+	let tipos = poke.types.map((type) => `<li class="${type.type.name}">${type.type.name}</li>`);
+	tipos = tipos.join("");
+	const div = document.createElement("div");
+	div.classList.add("pokemon");
+	div.innerHTML = `
         <div class="caja_sup">
           <img src="${poke.sprites.front_default}" alt="${poke.name}" />
           <div class="id">ID/${poke.id}</div>
@@ -27,33 +41,21 @@ function mostrarPokemon(poke) {
           <ul style="list-style-type: none">
             ${tipos}
           </ul>
+          ${
+						poke.evolucion
+							? `
           <div class="evolucion">
             Evoluciona de:
-            <span>${evolucion}</span>
-          </div>
+            <span>${poke.evolucion}</span>
+          </div>`
+							: `<div>
+            <span></br><br></span>
+          </div>`
+					}
         </div>
-  
     `;
-  listaPokemon.append(div);
+	listaPokemon.append(div);
 }
-
-/*
-
-        <div class="pokemon">
-          <div class="caja_sup">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" alt="Ivisaur" />
-            <div class="id">ID/2</div>
-          </div>
-          <div class="caja_inf">
-            <h3 class="pokemon-nombre">Ivisaur</h3>
-            <ul style="list-style-type: none">
-              <li>Poisson</li>
-              <li>Grass</li>
-            </ul>
-            <div>
-              Evoluciona de:
-              <span>Bulvasur</span>
-            </div>
-          </div>
-        </div>
-*/
+ function buscarPokemon(){
+    
+ }
