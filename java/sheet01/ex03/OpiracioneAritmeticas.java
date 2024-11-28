b6df0395f35878dc35235720004761852aed088b
jlvalladares <jvalladaresreina@gmail.com>
2024-11-28 14:04:09 +0100

    WIP: OperacionesAritmeticas.java

---
+++ java/sheet01/ex03/OpiracioneAritmeticas.java
@@ -0,0 +1,11 @@
+public class OperacionesAritmeticas{
+    public static void main(String[] args){
+        int a;
+        int b;
+        int suma = a+b;
+        int resta = a-b;
+        int multi = a*b;
+        int divi = a/b;
+        
+    }
+}
