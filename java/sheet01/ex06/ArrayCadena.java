3e93bfae59d955d172c705cef62521934d7840c4
jlvalladares <jvalladaresreina@gmail.com>
2024-11-29 19:47:14 +0100

    Jugando con arrays

---
+++ java/sheet01/ex06/ArrayCadena.java
@@ -0,0 +1,8 @@
+public class ArrayCadena {
+    public static void main(String[] args){
+        String [] nombres = {"Pedro", "juan", "Botijo", "Grabie", "Pancracio"};
+        for (int i= 0; i< nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
---
+++ java/sheet01/ex06/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays{
+    public static void main(String[] args){
+        int i;
+        int [] numeros = {1, 2, 3, 4, 5};
+        for (i= 0; i< numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+    }
+}
