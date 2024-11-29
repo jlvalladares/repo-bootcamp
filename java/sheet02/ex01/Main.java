b16c4689b6f34daf2215081a43135cd76a5219ce
jlvalladares <jvalladaresreina@gmail.com>
2024-11-29 13:08:41 +0100

    Fixed separndo la clase Main de class Persona

---
+++ java/sheet02/ex01/Main.java
@@ -0,0 +1,7 @@
+class Main {
+
+    public static void main(String[] args){
+        Persona persona = new Persona("Juan", 26);
+        persona.mostrarDetalles();
+    }
+}
--- java/sheet02/ex01/Persona.java
+++ java/sheet02/ex01/Persona.java
@@ -14,11 +14,3 @@ class Persona {
     }
 
 }
-
-class Main {
-
-    public static void main(String[] args){
-        Persona persona = new Persona("Juan", 26);
-        persona.mostrarDetalles();
-    }
-}
