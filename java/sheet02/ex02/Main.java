71941d3e22a101b6653241a0b8ba51bad91cfb15
jlvalladares <jvalladaresreina@gmail.com>
2024-11-29 13:55:21 +0100

    que zi

--- java/sheet02/ex02/Main.java
+++ java/sheet02/ex02/Main.java
@@ -5,11 +5,5 @@ class Main {
         persona.setNombre("Carlos");
         persona.setEdad(30);
         persona.mostrarDetalles();
-        System.out.print(persona.getNombre());
-        if (persona.getEdad() >= 18) {
-            System.out.println(" es mayor de edad");
-        } else {
-            System.out.println(" es menor de edad");
-        }
     }
 }
